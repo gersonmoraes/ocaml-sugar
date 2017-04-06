2d88e1b83adb9f30303e9755c9d62e55c1bea0e1
Gerson <gerson.xp@gmail.com>
2017-04-06 18:32:25 -0300

    Updating interfaces

--- examples/free/free_error_aware_combined.ml
+++ examples/free/free_error_aware_combined.ml
@@ -1,6 +1,4 @@
-(* open Sugar *)
-
-module Machine = Sugar_machine_error_aware
+open Sugar
 open Machine.Utils
 open Printf
 
@@ -10,11 +8,6 @@ module Terminal = struct
   module Error = struct
     type error = unit
   end
-
-  (* module Computation = Sugar.MakeResult
-    (struct
-      type error = unit
-    end) *)
 
   (*
      If we add errors and results to the Core module, we can figure them out on
@@ -68,8 +61,72 @@ let _ =
 
 
 
+module Terminal2 = struct
+
+  module Error = struct
+    type error = string
+  end
+
+  (*
+     If we add errors and results to the Core module, we can figure them out on
+     combine modules as well, right?
+  *)
+  module Core = struct
+
+    (* Our new convention *)
+    include Sugar.MakeResult (Error)
+
+    type 'f t =
+      | Puts2 of string * ('f, unit result) next
+      | GetLine2 of ('f, string result) next
+
+    let map f = function
+      | Puts2 (s, g) -> Puts2 (s, f @ g)
+      | GetLine2 g -> GetLine2 (f @ g)
+  end
+  open Core
+
+  module Spec = Machine.SpecFor (Core)
+
+  module Dsl (Ctx:Spec.S.Context) = struct
+    open Ctx
+    module Result = For(Free)
+
+    let puts s =
+      Puts2 (s, id) |> lift
+
+    let get_line () =
+      GetLine2 id |> lift
+  end
+
+  module Runner = struct
+    let run = function
+      | Puts2 (s, f) -> print_endline s; commit () |> f
+      | GetLine2 f -> read_line () |> commit |> f
+
+    let debug = function
+      | Puts2 (s, f) ->
+          printf "Puts: %s\n" s; commit () |> f
+      | GetLine2 f ->
+          printf "GetLine: ";
+          read_line () |> commit |> f
+  end
+end
+(*
+let _ =
+  (module Terminal.Core:Machine.Language),
+  (module Terminal:Machine.Runtime) *)
+
+
+module Terminals = struct
+  include Machine.Combine (Terminal.Core) (Terminal2.Core)
+end
+
+(* module Terminals. *)
+
 module MyMachine = Machine.For(Terminal)
 module TerminalDsl = Terminal.Dsl (MyMachine)
+(* module TerminalDsl2 = Terminal2.Dsl (MyMachine) *)
 
 open TerminalDsl
 open Result
